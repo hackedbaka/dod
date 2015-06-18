0f5b0f8840c55762399eddf1942857ffd0230b64
hackedbaka <jeffrey.wu23@gmail.com>
2015-06-17 17:25:43 -0700

    working?

---
+++ app/assets/javascripts/game.js
@@ -0,0 +1,98 @@
+// modal stuff
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
+
+// pusher stuff
+var channel_name='test_channel';
+var pusher = new Pusher('be43fa2ad18873862a59');
+var channel = pusher.subscribe(channel_name);
+    
+channel.bind('my_event', function(data) {
+      var answer = document.getElementById("answer");
+      answer.innerHTML = data.message;
+    });
+
+//stopwatch stuff
+var clsStopwatch = function() {
+    // Private vars
+    var startAt = 0;  // Time of last start / resume. (0 if not running)
+    var lapTime = 0;  // Time on the clock when last stopped in milliseconds
+ 
+    var now = function() {
+        return (new Date()).getTime(); 
+      }; 
+ 
+    // Public methods
+    // Start or resume
+    this.start = function() {
+        startAt = startAt ? startAt : now();
+      };
+ 
+    // Stop or pause
+    this.stop = function() {
+        // If running, update elapsed time otherwise keep it
+        lapTime = startAt ? lapTime + now() - startAt : lapTime;
+        startAt = 0; // Paused
+      };
+ 
+    // Reset
+    this.reset = function() {
+        lapTime = startAt = 0;
+      };
+ 
+    // Duration
+    this.time = function() {
+        return lapTime + (startAt ? now() - startAt : 0); 
+      };
+  };
+ 
+var x = new clsStopwatch();
+var time=$('#time').attr('value');
+var clocktimer;
+ 
+function pad(num, size) {
+  var s = "0000" + num;
+  return s.substr(s.length - size);
+}
+ 
+function formatTime(time) {
+  var h = m = s = ms = 0;
+  var newTime = '';
+ 
+  h = Math.floor( time / (60 * 60 * 1000) );
+  time = time % (60 * 60 * 1000);
+  m = Math.floor( time / (60 * 1000) );
+  time = time % (60 * 1000);
+  s = Math.floor( time / 1000 );
+  ms = time % 1000;
+ 
+  newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
+  return newTime;
+}
+ 
+function update() {
+  $('#time').text(formatTime(x.time()));
+}
+ 
+function start() {
+  clocktimer = setInterval("update()", 1);
+  x.start();
+}
+ 
+function stop() {
+  x.stop();
+  clearInterval(clocktimer);
+}
+ 
+function reset() {
+  stop();
+  x.reset();
+  update();
+}
+
+$('#button1').click(function(){
+  var params = "answer=" + "button1: " + x.time()/1000;    
+  var url = '/';
+  $.post(url,params,function(){ console.log("request complete")});
+});
