26e636d124f1ec1373f6dac376a852efe91421c8
hackedbaka <jeffrey.wu23@gmail.com>
2015-06-19 17:05:42 -0700

    figaro editing

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,8 +3,8 @@ OmniAuth.config.logger = Rails.logger
 provider :google_oauth2,
     ENV['OAUTH_CLIENT_ID'],
     ENV['OAUTH_CLIENT_SECRET'], {
-    # redirect_uri:"http://localhost:3000/auth/google_oauth2/callback"
-    redirect_uri:"https://doubleordonate.herokuapp.com/auth/google_oauth2/callback"
+    redirect_uri:"http://localhost:3000/auth/google_oauth2/callback"
+    # redirect_uri:"https://doubleordonate.herokuapp.com/auth/google_oauth2/callback"
 }
     
   # provider :google_oauth2, '730973355649-vu2o61sssrjbklu4fo6imilnufuf9g19.apps.googleusercontent.com', 'KAWMdEBZ5wUSovfM3N1QHiLj'
