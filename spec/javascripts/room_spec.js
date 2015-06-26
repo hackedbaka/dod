7cb0d49e15bacb6c6ec535e48484adc2febe0a6f
hackedbaka <jeffrey.wu23@gmail.com>
2015-06-26 14:53:05 -0700

    adding testing

---
+++ spec/javascripts/room_spec.js
@@ -0,0 +1,21 @@
+//preload the fixture so that it loads faster
+fixture.preload("room.html"); // make the actual requests for the files
+
+describe("Using fixtures", function() {
+
+
+    beforeEach(function() {
+        //setup the fixture that we have previously required
+        //initialize();
+        this.fixtures = fixture.load("room.html", true); // append these fixtures which were already cached
+    });
+
+    it("check buttons", function() {
+
+        expect($("#button1")).toBe(1);
+        
+
+    });
+
+
+});
